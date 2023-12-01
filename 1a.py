382e4e33885e2e0eaeb044a24093135ecca7e373
isirk <67282396+isirk@users.noreply.github.com>
2023-12-01 09:49:28 -0700

    solved 1a

---
+++ 1a.py
@@ -0,0 +1,14 @@
+input = open("1a_input.txt", "r")
+sum = 0
+
+for line in input:
+    nums = [int(x) for x in line if x.isdigit()]
+    if len(nums) == 1:
+        string = str(nums[0]) + str(nums[0])
+        sum += int(string)
+    else:
+        string = str(nums[0]) + str(nums.pop())
+        sum += int(string)
+
+input.close()
+print(sum)
