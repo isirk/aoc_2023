8253f4017ec51fffbb69ddbb1304d03b36f63599
isirk <67282396+isirk@users.noreply.github.com>
2023-12-01 12:38:11 -0700

    cant figure out why its wrong

--- 1b.py
+++ 1b.py
@@ -2,14 +2,16 @@ input = open("1_input.txt", "r")
 sum = 0
 
 numbers = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
-           "six": 6, "seven": 7, "eight": 8, "nine": 9,
-           "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9}
+           "six": 6, "seven": 7, "eight": 8, "nine": 9}
 
 def is_value(line):
-    return [val for key, val in numbers.items() if key in line]
+    for key in numbers.keys():
+        line = line.replace(key, str(numbers[key]))
+    return line
 
 for line in input:
-    nums = is_value(line)
+    values = is_value(line)
+    nums = [int(x) for x in values if x.isdigit()]
     if len(nums) == 1:
         string = str(nums[0]) + str(nums[0])
         sum += int(string)
